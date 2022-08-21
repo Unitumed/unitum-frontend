81cdd8181369ac5e4f6c8f6a7a55d07ddc3ddad6
John Barnes Oduro Twumasi <ohenesetwumasi@gmail.com>
2022-08-21 14:11:12 +0000

    Fix: Api route

--- src/lib/axios-init.ts
+++ src/lib/axios-init.ts
@@ -1,8 +1,5 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL:
-    process.env.NODE_ENV === "production"
-      ? process.env.API
-      : "http://localhost:8080/api/"
+  baseURL: process.env.REACT_APP_API
 });
